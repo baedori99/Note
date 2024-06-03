8040ecebe1b37d68ec10f199d37855a38786bd00
baedori99 <baejason36@gmail.com>
2024-06-03 17:43:37 +0900

    Changing page title

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "🪴 Quartz 4.0",
+    pageTitle: "🪴 뚝바이브 개발자",
     enableSPA: true,
     enablePopovers: true,
     analytics: {
